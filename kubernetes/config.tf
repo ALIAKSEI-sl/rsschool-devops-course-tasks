301c4ea58c7aaa92a12232364466a0093d55e4de
Aliaksei <sladkiy.aleksey@gmail.com>
2024-10-04 19:45:37 +0200

    feat: add VPC

---
+++ kubernetes/config.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.aws_region
+  map_public_ip_on_launch = true # instances launched into the subnet should be assigned a public IP address
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = var.aws_region
+  map_public_ip_on_launch = true # instances launched into the subnet should be assigned a public IP address
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = var.aws_region
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# Создаем вторую приватную подсеть
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = var.aws_region
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
